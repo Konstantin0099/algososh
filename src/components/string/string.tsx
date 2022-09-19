02354400a61039644d94cbdd1d458701c121ebcd
Konstantin_999 <sh-tov@yandex.ru>
2022-09-20 06:15:08 +0700

    отобразить строку

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { DELAY_IN_MS } from "../../constants/delays";
-import style from "./string.module.css"
-console.log("string");
+import style from "./string.module.css";
 const { page, input, string, letters, letter } = style;
 
 export const StringComponent: React.FC = () => {
-  return (
-    <SolutionLayout title="Строка_Строка">
-     <div className={page}>
-     StringComponent
-      <Input
-      />
-      <Button
-      />
-     </div>
-     <ul className={letters}>
+  const [isLoader, setIsLoader] = useState(false);
+  const [inputString, setInputString] = useState("");
+  const [arrLetters, setArrLetters] = useState<string[]>([]);
 
-     </ul>
+  const wrapString = useMemo(
+    () => (string: string) => {
+      setIsLoader(true);
+      setArrLetters(string.split(""));
+    },
+    [setIsLoader, setArrLetters]
+  );
+  const inputChange = useMemo(
+    () => (e: any) => setInputString(e.target.value),
+    [setInputString]
+  );
+  return (
+    <SolutionLayout title="Строка">
+      <div className={page}>
+        <Input
+          type={"text"}
+          maxLength={11}
+          value={inputString}
+          onChange={inputChange}
+        />
+        <Button
+          text="развернуть"
+          isLoader={false}
+          disabled={false}
+          linkedList={"big"}
+          onClick={() => wrapString(inputString)}
+        />
+      </div>
+      <ul className={letters}>
+        {arrLetters.map((letter, index) => (
+          <li key={index} className={letter}>
+            <Circle letter={letter} />
+          </li>
+        ))}
+      </ul>
     </SolutionLayout>
   );
 };

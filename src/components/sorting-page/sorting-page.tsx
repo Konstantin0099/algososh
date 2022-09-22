5ee1efa9d4be121df07e8c09312874573938d03d
Konstantin_999 <sh-tov@yandex.ru>
2022-09-22 14:17:55 +0700

    отобразить интерфейс сортировки

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useMemo} from "react";
+import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Column } from "../ui/column/column";
+import { DELAY_IN_MS } from "../../constants/delays";
+import { ARR_CONSTANTS } from "../../constants/arr";
+import style from "./sorting-page.module.css";
+
+const {page, buttonsBox, box} = style;
+
 
 export const SortingPage: React.FC = () => {
   return (
     <SolutionLayout title="Сортировка массива">
+<div className={page}>
+<div className={buttonsBox}>
+  <div className={box}>
+    <RadioInput
+     label="Выбор"
+    />
+        <RadioInput
+label="Пузырек"
+/>
+  </div>
+  <div className={box}>
+    <Button 
+    text="По возрастанию"
+    />
+     <Button 
+     text="По убыванию"
+    />
+  </div>
+  <Button 
+     text="Новый массив"
+    />
+</div>
 
+</div>
     </SolutionLayout>
   );
 };
---
+++ src/constants/arr.ts
@@ -0,0 +1,6 @@
+export const ARR_CONSTANTS = {
+    minValue: 0,
+    maxValue: 100,
+    minSize: 3,
+    maxSize: 17
+}

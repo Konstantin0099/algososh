6c7cb951614047ec6766499c9772b2e340c479cc
Konstantin_999 <sh-tov@yandex.ru>
2022-09-25 23:15:34 +0700

    реализовать функционал СТЕКА

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
@@ -12,14 +12,17 @@ import {
   ButtonState,
 } from "../../types";
 import style from "./stack-page.module.css";
-import {Stack} from "./stack";
+import { Stack, ElementArray, Node } from "./stack";
+import { render } from "@testing-library/react";
 //import { clear } from "console";
 // import { arrayBuffer } from "stream/consumers";
 
-
 export const StackPage: React.FC = () => {
-  const { page, input, buttonsBox , node, clear, button } = style;
+  const { page, input, buttonsBox, node, clear, button, nodeBox } = style;
   const [inputValue, setInputValue] = useState("");
+  const [renderArray, setrenderArray] = useState<ElementArray<string>[] | []>(
+    []
+  );
   const [stack, setStack] = useState(new Stack<string>());
   const [buttonState, setBattonState] = useState<ButtonState>({
     add: {
@@ -32,12 +35,103 @@ export const StackPage: React.FC = () => {
       isDisabled: true,
     },
   });
-  const inputChange = () => {};
-  const pushStack = () => {};
-  const popStack = () => {};
-  const clearStack = () => {};
 
+  useEffect(() => {
+    // const renderArray: ElementArray<T>[] = [];
+  }, []);
 
+  const renderStack = () => {
+    // console.log("renderStack");
+    const nodeTemp = stack.head;
+    nodeTemp && nodeTemp.state && (nodeTemp.state = ElementStates.Default);
+    const arrTemp = stack.toArray();
+    // nodeTemp && nodeTemp.state && console.log("renderStack", nodeTemp, arrTemp);
+    setrenderArray(arrTemp);
+    setTimeout(() => {
+      // console.log("setTimeout", stack.arr.length - 1);
+
+      // stack.head && (stack.head.state = ElementStates.Default);
+      stack.arr.length >= 1 &&
+        (stack.arr[stack.arr.length - 1].state = ElementStates.Default);
+      // setrenderArray([...stack.toArray()]);
+      setrenderArray([...arrTemp]);
+      // console.log("перекраска", nodeTemp, arrTemp);
+    }, 500);
+  };
+
+  // const inputChange = (
+
+  // ) => {};
+
+  const inputChange = (e: any) => {
+    // let b1 = buttonState;
+    // console.log("inputChange1", b1);
+    setInputValue(e.target.value);
+    !e.target.value &&
+      setBattonState({ ...buttonState, add: { isDisabled: true } });
+    e.target.value &&
+      setBattonState({ ...buttonState, add: { isDisabled: false } });
+    console.log("buttonState", buttonState);
+    // let b2 = buttonState;
+    // console.log("inputChange2", b2);
+    // if (e.target.value) {
+    //   setIsDisabled(false);
+    // } else {
+    //   setIsDisabled(true);
+    // }
+  };
+
+  const pushStack = (value: string) => {
+    let b1 = buttonState;
+    if (stack) {
+      // console.log("pushStack", value);
+      stack.push(value);
+    } else {
+      setStack(new Stack(value));
+    }
+    setInputValue("");
+    // let b1 = buttonState;
+    // console.log("inputChange1", b1);
+    setBattonState({
+      ...{
+        clear: { isDisabled: false },
+        add: { isDisabled: true },
+        del: { isDisabled: false },
+      },
+    });
+    let b2 = { ...buttonState };
+    // console.log("b2 === b1", b2 === b1, b2, b1);
+    renderStack();
+
+  };
+
+  const popStack = () => {
+    stack.arr.length >= 1 &&
+    (stack.arr[stack.arr.length - 1].state = ElementStates.Changing);
+    renderStack();
+   
+    setTimeout(() => {
+    stack.pop();
+    renderStack();
+    stack.arr.length === 0 &&
+      setBattonState({
+        ...buttonState,
+        clear: { isDisabled: true },
+        del: { isDisabled: true },
+      });
+    }, 500)
+   
+  };
+  const clearStack = () => {
+  stack.clear();
+    renderStack();
+    setBattonState({
+      add: { isDisabled: true },
+      del: { isDisabled: true },
+      clear: { isDisabled: true },
+    });
+
+  };
 
   return (
     <SolutionLayout title="Стек">
@@ -55,7 +149,7 @@ export const StackPage: React.FC = () => {
           // isLoader={isLoader}
           disabled={buttonState.add.isDisabled}
           // linkedList={"small"}
-          onClick={() => pushStack()}
+          onClick={() => pushStack(inputValue)}
           extraClass={button}
         />
         <Button
@@ -75,18 +169,21 @@ export const StackPage: React.FC = () => {
           extraClass={clear}
         />
       </div>
-      <ul className={buttonsBox }>
-        {stack.arr.map((i, index, arr) => (
-          <li key={index} className={i.state}>
-            <Circle
-              extraClass={node}
-              state={i.state}
-              letter={i.value}
-              index={index}
-              head={index === arr.length - 1 ? "top" : ""}
-            />
-          </li>
-        ))}
+      <ul className={nodeBox}>
+        {renderArray &&
+          renderArray.map((i, index, arr) => (
+            <li key={index} className={i.state}>
+              {/* {inputValue} */}
+              <Circle
+                extraClass={node}
+                state={i.state}
+                letter={i.value}
+                index={index}
+                // index={index}
+                head={index === arr.length - 1 ? "top" : ""}
+              />
+            </li>
+          ))}
       </ul>
     </SolutionLayout>
   );
--- src/components/stack-page/stack.ts
+++ src/components/stack-page/stack.ts
@@ -1,10 +1,10 @@
 import { ElementStates } from "../../types";
 export class ElementArray<T> {
-  value: T;
+  value?: T;
   state: ElementStates;
   constructor(value: T) {
     this.value = value;
-    this.state = ElementStates.Default;
+    this.state = ElementStates.Changing;
   }
 }
 export class Node<T> extends ElementArray<T> {
@@ -25,23 +25,28 @@ export interface IStack<T> {
 }
 
 export class Stack<T> implements IStack<T> {
-  private head: Node<T> | null;
+  head: Node<T> | null;
   private size: number;
   arr: ElementArray<T>[];
-  constructor() {
+  constructor(value?: T) {
     this.head = null;
     this.size = 0;
     this.arr = [];
   }
   push = (element: T): void => {
-    const newNode = new Node(element);
-    newNode.next = this.head;
-    this.head = newNode;
-  };
+      const newNode = new Node(element);
+      newNode.next = this.head;
+      this.head = newNode;
+      this.arr = [...this.arr, {value: element, state: ElementStates.Changing}]
+   };
   pop = (): void => {
-    this.head !== null && this.head.next !== null
+    //   console.log("pop", this.arr)
+      this.head !== null && this.head.next !== null
       ? (this.head = this.head.next)
       : (this.head = this.head);
+      this.arr.pop();
+    //   console.log("pop", this.arr.length);
+    //   console.log("pop1", this.arr)
   };
   peak = (): Node<T> | null => {
     return this.head;
@@ -52,7 +57,8 @@ export class Stack<T> implements IStack<T> {
     this.arr = [];
   };
   getSize = (): number => {
-    return this.size;
+    // console.log("getSize", this.arr.length);
+    return this.arr.length;
   };
   toArray = (): ElementArray<T>[] => {
     return this.arr;
